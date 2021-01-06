0c517b34a319d6e70c99b1cfea44640849900e9f
anabneri <$git_config_user_email>
2021-01-06 11:46:27 -0300

    first version project

---
+++ src/main/java/com/globomantics/inventorymanager/InventorymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.globomantics.inventorymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorymanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globomantics/inventorymanager/InventorymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globomantics.inventorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
